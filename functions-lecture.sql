534a577ff35a1a8af8ec46fdade6c5359fcd5b71
Fernando Mendoza <fernandoza@gmail.com>
2019-08-15 14:59:40 -0500

    update: better format

--- functions-lecture.sql
+++ functions-lecture.sql
@@ -1,14 +1,17 @@
-select concat_ws(' ',first_name, last_name, emp_no) from employees;
+select concat_ws(' ', first_name, last_name, emp_no)
+from employees;
 
-select concat(first_name, ' ',last_name, ' | ',emp_no) from employees;
+select concat(first_name, ' ', last_name, ' | ', emp_no)
+from employees;
 
-select first_name + ' ' + last_name + ' | ' + emp_no from employees;
+select first_name + ' ' + last_name + ' | ' + emp_no
+from employees;
 
 select now();
 select curdate();
 
 SELECT CONCAT(
                'Teaching people to code for ',
-               ((((((UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04')) / 60 ) / 60) / 24) / 30) / 12) ,
+               ((((((UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04')) / 60) / 60) / 24) / 30) / 12),
                ' '
            );
